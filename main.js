7137c52766abdfb18c614c3f0cddb7dfba2e767d
xomril <omri@alink.co.il>
2020-08-27 14:32:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -112,9 +112,9 @@ grid= currentGrid;
     for(x=0;x<grid.length;x++){
         for(y=0;y<grid[x].length;y++){
             if(grid[x][y]==1){
-                cell="*"
+                cell="1"
             } else {
-                cell=" "
+                cell="0"
             }
             document.querySelector("#board").innerHTML +=cell;
         }
